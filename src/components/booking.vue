7e24c1b46d0047010c73992e8837e6c8517b699e
Pablo Ledesma <pabloledes83@gmail.com>
2017-01-10 23:33:43 -0500

    se crearon los objetos Form y Errors | finalización de los campos | validación en proceso...

--- src/components/booking.vue
+++ src/components/booking.vue
@@ -4,53 +4,83 @@
 		<form @submit.prevent="onSubmit">
 			<p class="control">
 			  <label class="radio">
-			    <input type="radio" name="typeOfTrip" value="RT" v-model="typeOfTrip">
+			    <input type="radio" name="typeOfTrip" value="RT" v-model="form.typeOfTrip">
 			    Ida y vuelta
 			  </label>
 			  <label class="radio">
-			    <input type="radio" name="typeOfTrip" value="OW" v-model="typeOfTrip">
+			    <input type="radio" name="typeOfTrip" value="OW" v-model="form.typeOfTrip">
 			    Una Vía
 			  </label>
 			</p>
 
 			<p class="control">
 			  <span class="select">
-			    <select v-model="origin">
+			    <select v-model="form.origin" required>
 			      <option value="">Desde</option>
 			      <option v-for="city in fromCities" :value="city.id">{{city.name}}</option>
 			    </select>
+			    <span class="help is-danger" v-text="form.errors.get('origin')" v-if="form.errors.has('origin')"></span>
 			  </span>
 
 			  <span class="select">
-			    <select v-model="destination">
+			    <select v-model="form.destination">
 			      <option value="">Hacia</option>
 			      <option v-for="city in toCities" :value="city.id">{{city.name}}</option>
 			    </select>
 			  </span>
 			</p>
 
-			<p class="control">
+			<p class="controsrcl">
 			  <label class="radio">
-			    <input type="radio" name="classOfTrip" value="business" v-model="classOfTrip">
+			    <input type="radio" name="classOfTrip" value="business" v-model="form.classOfTrip">
 			    Clase ejecutiva
 			  </label>
 			  <label class="radio">
-			    <input type="radio" name="classOfTrip" value="economy" v-model="classOfTrip">
+			    <input type="radio" name="classOfTrip" value="economy" v-model="form.classOfTrip">
 			    Clase económica
 			  </label>
 			</p>
 
-			<p class="control">
-				<label class="label">Salida</label>
-				<datepicker v-model="departureDate" :disabled="disabled" language="es"></datepicker>
-				<label class="label">Regreso</label>
-				<datepicker v-model="returnDate" :disabled="disabled" language="es"></datepicker>
-			</p>
+			<div class="control is-grouped">
+				<div class="control">
+					<label class="label">Salida</label>
+					<datepicker v-model="form.departureDate" :disabled="disabled" language="es"></datepicker>
+				</div>
+				<div v-if="typeOfTrip == 'RT'" class="control">
+					<label class="label">Regreso</label>
+					<datepicker v-model="form.returnDate" :disabled="disabled" language="es"></datepicker>
+				</div>
+			</div>
 
 			<div class="control is-grouped">
-			  <p class="control">
+				<div class="control">
+				  	<label class="label">Adultos<br>(12+)</label>
+				  	<span class="select">
+				    	<select v-model="form.adults">
+				      		<option v-for="number in numbers" :value="number">{{number}}</option>
+				    	</select>
+				  	</span>
+				</div>
+				<div class="control">
+					<label class="label">Niños<br>(2-11)</label>
+				  	<span class="select">
+				    	<select v-model="form.childs">
+				      		<option v-for="number in numbers" :value="number">{{number}}</option>
+				    	</select>
+				  	</span>
+				</div>
+				<div class="contro">
+					<label class="label">Infantes<br>(0-1)</label>
+				   	<span class="select">
+				    	<select v-model="form.infants">
+				      		<option v-for="number in numbers" :value="number">{{number}}</option>
+				    	</select>
+				  	</span>
+				</div>
+			</div>
+
+			<div class="control">
 			    <button class="button is-primary">Buscar Vuelo</button>
-			  </p>
 			</div>
 			
 		</form>
@@ -59,19 +89,26 @@
 <script>
 import info from '../data/info'
 import Datepicker from 'vuejs-datepicker'
+import Form from '../core/Form'
 
 	export default {
 		name: 'booking',
 		data(){
 			return {
-				typeOfTrip: 'OW',
-				origin: '',
-				destination: '',
-				classOfTrip: 'economy',
-				departureDate: '',
-				returnDate: '',
+				form: new Form({
+					typeOfTrip: 'OW',
+					origin: '',
+					destination: '',
+					classOfTrip: 'economy',
+					departureDate: '',
+					returnDate: '',
+					adults: 1,
+					childs: 0,
+					infants: 0
+				}), 
 				fromCities: info.fromcities,
 				toCities: info.tocities,
+				numbers: [0,1,2,3,4,5,6,7,8],
 				disabled: {
 					to: info.departure_date,
 					from: info.return_date
@@ -81,8 +118,11 @@ import Datepicker from 'vuejs-datepicker'
 		components: {
 			Datepicker
 		},
-		mounted(){
-			//console.log(info);
+		methods:{
+			onSubmit(){
+				console.log('submited!');
+			}
 		}
+		
 	}
 </script>
