6b7763577a3600862687274b6660da360e2683f8
Pabloledesma <=>
2017-01-11 14:37:37 -0500

    validación del formulario con vee-validate

--- src/components/booking.vue
+++ src/components/booking.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="column">
 		<h3>Reserva tu vuelo ahora</h3>
-		<form @submit.prevent="onSubmit">
+		<form @submit.prevent="validateBeforeSubmit">
 			<p class="control">
 			  <label class="radio">
 			    <input type="radio" name="typeOfTrip" value="RT" v-model="typeOfTrip">
@@ -13,21 +13,39 @@
 			  </label>
 			</p>
 
-			<p class="control">
-			  <span class="select">
-			    <select v-model="origin">
-			      <option value="">Desde</option>
-			      <option v-for="city in fromCities" :value="city.id">{{city.name}}</option>
-			    </select>
-			  </span>
+			<div class="control is-grouped">
+				<div class="control">
+				  	<span class="select">
+					    <select 
+					    	v-model="origin" 
+					    	v-validate 
+					    	data-vv-rules="required" 
+					    	name="origin"
+							:class="{'input': true, 'is-danger': errors.has('origin') }"
+					    >
+					      	<option value="">Desde</option>
+					      	<option v-for="city in fromCities" :value="city.id">{{city.name}}</option>
+					    </select>
+				  	</span>
+					<span v-show="errors.has('origin')" class="help is-danger">{{ errors.first('origin') }}</span>
+				</div>
 
-			  <span class="select">
-			    <select v-model="destination">
-			      <option value="">Hacia</option>
-			      <option v-for="city in toCities" :value="city.id">{{city.name}}</option>
-			    </select>
-			  </span>
-			</p>
+				<div class="control">
+				  	<span class="select">
+					    <select 
+					    	v-model="destination" 
+					    	v-validate 
+					    	data-vv-rules="required" 
+					    	name="destination"
+							:class="{'input': true, 'is-danger': errors.has('destination') }"
+					    >
+					      	<option value="">Hacia</option>
+					      	<option v-for="city in toCities" :value="city.id">{{city.name}}</option>
+					    </select>
+				  	</span>
+					<span v-show="errors.has('destination')" class="help is-danger">{{ errors.first('destination') }}</span>
+				</div>
+			</div>
 
 			<p class="control">
 			  <label class="radio">
@@ -40,17 +58,42 @@
 			  </label>
 			</p>
 
-			<p class="control">
-				<label class="label">Salida</label>
-				<datepicker v-model="departureDate" :disabled="disabled" language="es"></datepicker>
-				<label class="label">Regreso</label>
-				<datepicker v-model="returnDate" :disabled="disabled" language="es"></datepicker>
-			</p>
-
 			<div class="control is-grouped">
-			  <p class="control">
+				<div class="control">
+					<label class="label">Salida</label>
+					<datepicker 
+						v-model="departureDate"
+						v-validate
+						data-vv-rules="required" 
+						:disabled="disabled" 
+						language="es"
+						name="departureDate"
+						:class="{'input': true, 'is-danger': errors.has('departureDate') }"
+					></datepicker>
+					<span v-show="errors.has('departureDate')" class="help is-danger">
+						{{ errors.first('departureDate') }}
+					</span>
+				</div>
+
+				<div class="control" v-if="typeOfTrip == 'RT'">
+					<label class="label">Regreso</label>
+					<datepicker 
+						v-model="returnDate" 
+						v-validate
+						data-vv-rules="required"
+						:disabled="disabled" 
+						language="es"
+						name="returnDate"
+						:class="{'input': true, 'is-danger': errors.has('returnDate') }"
+					></datepicker>
+					<span v-show="errors.has('returnDate')" class="help is-danger">
+						{{ errors.first('returnDate') }}
+					</span>
+				</div>
+			</div>
+
+			<div class="control">
 			    <button class="button is-primary">Buscar Vuelo</button>
-			  </p>
 			</div>
 			
 		</form>
@@ -81,8 +124,17 @@ import Datepicker from 'vuejs-datepicker'
 		components: {
 			Datepicker
 		},
-		mounted(){
-			//console.log(info);
+		methods: {
+			validateBeforeSubmit() {
+            	// Validate All returns a promise and provides the validation result.                
+	            this.$validator.validateAll().then(success => {
+	                if (! success) {
+	                    // handle error
+	                    return;
+	                }
+	                alert('From Submitted!');
+	            });
+        	}
 		}
 	}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VeeValidate from 'vee-validate'
 
 Vue.use(VueResource)
+Vue.use(VeeValidate)
+
+/**
+* Validation messages
+*/
+const dictionary = {
+	en: {
+        city_origin_null: "Select a city of origin",    
+        destination_city_null: "Select the destination city",
+        origin_destination: "The destination city should be different from the city of origin",
+        departure_date: "Select departure",
+        return_date: "Select Return Date"
+  	},
+  	es: {
+        city_origin_null: "Seleccione la ciudad de origen",
+        destination_city_null: "Seleccione la ciudad de destino",
+        origin_destination: "La ciudad de destino debe ser diferente de la de origen",
+        departure_date: "Seleccione la fecha de salida",
+        return_date: "Seleccione la fecha de regreso"
+  	},
+  	pt: {
+        city_origin_null: "Selecione uma cidade de origem",
+        destination_city_null: "Selecione a cidade de destino",
+        origin_destination: "A cidade de destino deve ser diferente a partir da cidade de origem",
+        departure_date: "Selecione a partida",
+        return_date: "Selecionar data de retorno"
+  	}
+};
+
+//const validator = new VeeValidate();
 
 new Vue({
   el: '#app',
